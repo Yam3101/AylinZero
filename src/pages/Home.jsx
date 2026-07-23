53a21037cdb06b5c9d98501c6b4fc130e6d1fd72
yam <angelmay_810@hotmail.com>
2026-07-23 09:01:23 -0500

    UI issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,7 +25,7 @@ const socials = [
 const Home = () => {
 	return (
 		<div className="min-h-screen bg-degraded1 flex justify-center items-center px-6 py-10">
-			<div className="bg-white border-3 border-black shadow-[10px_10px_0px_#000] rounded-md w-full max-w-5xl overflow-hidden">
+			<div className="bg-white border-3 border-black shadow-[10px_10px_0px_#000] rounded-md w-full max-w-5xl overflow-x-hidden">
 				<div className="grid md:grid-cols-2 gap-12 p-10">
 					<div className="flex flex-col justify-center">
 						<img
