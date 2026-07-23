d4d050865f0a26588145d5b20b630b5b69752644
yam <angelmay_810@hotmail.com>
2026-07-23 10:41:02 -0500

    UI stuff

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
 
 	return (
 		<div className="min-h-screen bg-degraded1 flex justify-center items-center px-6 py-10">
-			<div className="bg-white border-3 border-black shadow-[10px_10px_0px_#000] rounded-md sm:w-3/4 w-9/10 overflow-hidden">
+			<div className="bg-white border-3 border-black shadow-[10px_10px_0px_#000] rounded-md sm:w-7/8 w-9/10 overflow-hidden">
 				<div className="grid md:grid-cols-2 gap-12 p-10">
 					<div className="flex flex-col justify-center">
 						<img
