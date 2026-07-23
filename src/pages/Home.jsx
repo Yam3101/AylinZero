6c70cb8549d8f60c5983f2308ba72fac4520c0d7
yam <angelmay_810@hotmail.com>
2026-07-23 10:23:26 -0500

    UI fix 2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -34,7 +34,7 @@ const Home = () => {
 
 	return (
 		<div className="min-h-screen bg-degraded1 flex justify-center items-center px-6 py-10">
-			<div className="bg-white border-3 border-black shadow-[10px_10px_0px_#000] rounded-md w-full max-w-5xl overflow-hidden">
+			<div className="bg-white border-3 border-black shadow-[10px_10px_0px_#000] rounded-md w-2/4 overflow-hidden">
 				<div className="grid md:grid-cols-2 gap-12 p-10">
 					<div className="flex flex-col justify-center">
 						<img
@@ -80,7 +80,7 @@ const Home = () => {
 									gap-4
 									Caveat-Brush
 									cursor-pointer
-									w-2/3
+									w-9/10
 								"
 							>
 								<span className="text-xl duration-200">{social.icon}</span>
